c11452fc987cbf87cb4ac1c8313a4332b3dfff2a
shuja <shuja.devops@gmail.com>
2024-12-11 11:21:58 +0530

    brackets fixed for list

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "tools_details" {
   default = {
     prometheus = {
       instance_type         = "t3.small"
-      policy_resource_list  = "ec2:DescribeInstances"
+      policy_resource_list  = ["ec2:DescribeInstances"]
     }
   }
 }

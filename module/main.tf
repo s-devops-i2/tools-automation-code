5f43224ae604326a3789feaf1922679413f440ed
shuja <shuja.devops@gmail.com>
2024-12-11 08:40:22 +0530

    ip updated

--- module/main.tf
+++ module/main.tf
@@ -15,16 +15,17 @@ resource "aws_route53_record" "record" {
   records = [aws_instance.instance.public_ip]
   ttl     = 30
 }
+
 resource "aws_iam_role" "prometheus_role" {
   name =  "${var.name}-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version   = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Sid       = ""
         Principal = {
           Service = "ec2.amazonaws.com"
         }
@@ -35,4 +36,9 @@ resource "aws_iam_role" "prometheus_role" {
   tags = {
     tag-key = "${var.name}-role"
   }
+}
+
+resource "aws_iam_instance_profile" "prom_inst_profile" {
+  name = "${var.name}-role"
+  role = aws_iam_role.prometheus_role.name
 }

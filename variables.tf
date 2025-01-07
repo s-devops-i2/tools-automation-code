ac737dbb7bd7ae0a4fa6df8be77354f8c4889dd0
shuja <shuja.devops@gmail.com>
2025-01-07 15:38:57 +0530

    uncommented prometheus and grafana

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "tools_details" {
   default = {
-#     prometheus = {
-#       instance_type         = "t3.small"
-#       policy_resource_list  = ["ec2:DescribeInstances"]
-#     }
-#     Grafana = {
-#       instance_type         = "t3.small"
-#       policy_resource_list  = []
-#
-#     }
+    prometheus = {
+      instance_type         = "t3.small"
+      policy_resource_list  = ["ec2:DescribeInstances"]
+    }
+    Grafana = {
+      instance_type         = "t3.small"
+      policy_resource_list  = []
+
+    }
     vault = {
       instance_type         = "t3.small"
       policy_resource_list  = []
